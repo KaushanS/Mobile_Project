8ba1d875202c3215b056929cde502607c3b05cf1
Pinidi20061204 <pinidi20231464@iit.ac.lk>
2026-03-05 14:42:59 +0530

    UI commit

---
+++ app/src/main/java/com/example/mobileproject/warranty/EditWarrantyActivity.java
@@ -0,0 +1,26 @@
+package com.example.mobileproject.warranty;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.mobileproject.R;
+
+public class EditWarrantyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_edit_warranty);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
