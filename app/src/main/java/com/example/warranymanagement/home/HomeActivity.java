d505531428189dcd9e29d3aefd8d8f286fae736d
KaushanS <157549588+KaushanS@users.noreply.github.com>
2026-03-08 02:57:05 +0530

    Update ui without errors

--- app/src/main/java/com/example/warranymanagement/home/HomeActivity.java
+++ app/src/main/java/com/example/warranymanagement/home/HomeActivity.java
@@ -17,5 +17,38 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        findViewById(R.id.AddWarranty).setOnClickListener(v -> {
+            Intent intent = new Intent(this, AddWarrantyActivity.class);
+            startActivity(intent);
+        });
+
+        findViewById(R.id.Notification).setOnClickListener(v -> {
+            Intent intent = new Intent(this, NotificationsActivity.class);
+            startActivity(intent);
+        });
+
+        BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
+        navView.setSelectedItemId(R.id.nav_home);
+
+        navView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_home) {
+                return true;
+            } else if (itemId == R.id.nav_mine) {
+                Intent intent = new Intent(this, WarrantyListActivity.class);
+                startActivity(intent);
+                return true;
+
+            } else if (itemId == R.id.nav_community) {
+                Intent intent = new Intent(this, CommunityActivity.class);
+                startActivity(intent);
+                return true;
+            } else if (itemId == R.id.nav_settings) {
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            }
+            return false;
+        });
     }
 }
